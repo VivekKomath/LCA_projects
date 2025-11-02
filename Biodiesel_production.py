d8cfca3ae2c6032847925a4050e10c5587b18945
VivekKomath <vivekchandran98@gmail.com>
2025-11-02 23:47:38 +0530

    Bio-diesel production. Need to implement utilities, transport and energy. Need to implement alternatives.

--- Biodiesel_production.py
+++ Biodiesel_production.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 
+# Set pandas display options to show all columns and prevent line wrapping
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
 def biodiesel_raw_materials(required_kg_biodiesel):
     # Molar masses (g/mol)
     M_triolein = 885.44
@@ -22,22 +26,26 @@ def biodiesel_raw_materials(required_kg_biodiesel):
     return mass_triolein, mass_ethanol, mass_glycerol
 
 
-raw_materials_df = pd.read_csv('raw_material_emissions.csv', index_col='Component')
-raw_materials_df['Value'] = ''
+raw_materials_df = pd.read_csv('raw_material_emissions.csv', index_col='Component',encoding='utf-8-sig')
+raw_materials_df.insert(0, 'Value', 0)
 
-utilities_df = pd.read_csv('utility_emissions.csv', index_col='Component')
-utilities_df['Value'] = ''
+utilities_df = pd.read_csv('utility_emissions.csv', index_col='Component', encoding='utf-8-sig')
+utilities_df.insert(0, 'Value', 0)
+
 products_df = pd.DataFrame(columns=raw_materials_df.columns)
+
 
 
 # Functional unit: 1 kg of biodiesel
 required_kg_biodiesel = 1
-mass_triolein, mass_bio_ethanol, mass_glycerol = biodiesel_raw_materials(required_kg_biodiesel)
+mass_triolein, mass_ethanol, mass_glycerol = biodiesel_raw_materials(required_kg_biodiesel)
+raw_materials_df.loc['Triolein', 'Value'] = mass_triolein
+raw_materials_df.loc['Bio-ethanol', 'Value'] = mass_ethanol
 
-results_triolein = raw_materials_df.loc['Triolein'] * mass_triolein
-results_bio_ethanol = raw_materials_df.loc['Bio-ethanol'] * mass_bio_ethanol
+results_triolein = raw_materials_df.loc['Triolein']* raw_materials_df.loc['Triolein', 'Value']
+results_bio_ethanol = raw_materials_df.loc['Bio-ethanol']* raw_materials_df.loc['Bio-ethanol', 'Value']
 total_emissions = results_triolein + results_bio_ethanol
-print(total_emissions)
+
 mass_allocation_glycerol = mass_glycerol / (mass_glycerol + required_kg_biodiesel)
 mass_allocation_biodiesel = required_kg_biodiesel / (mass_glycerol + required_kg_biodiesel)
 mass_results_glycerol = total_emissions * mass_allocation_glycerol
@@ -46,9 +54,13 @@ mass_results_biodiesel = total_emissions * mass_allocation_biodiesel
 # Store the allocated results in the products_df DataFrame
 products_df.loc['Biodiesel'] = mass_results_biodiesel
 products_df.loc['Glycerol'] = mass_results_glycerol
+products_df.at['Biodiesel', 'Value'] = required_kg_biodiesel
+products_df.at['Glycerol', 'Value'] = mass_glycerol
 
-# The 'Value' column is not needed for the final product results
-products_df = products_df.drop(columns=['Value'])
+products_df.at['Biodiesel', 'Cost (USD)'] = 1.4
+products_df.at['Glycerol', 'Cost (USD)'] = 1
+
+
 
 print("--- Allocated Emissions for Products ---")
 print(products_df)
